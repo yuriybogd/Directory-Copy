ccd2abc44c27085446457eec144c8e51fee0b1fa
Yury <yuriy.bogd@yandex.ru>
2020-02-08 01:06:03 +0300

    added guides's carousel

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,28 @@ $( document ).ready(function() {
     // AOS init
     AOS.init();
 
+    // OWL Carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:15,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
    
 
 
